caeab585e718e3c6987e7204a9e898067f7c76ed
Evan <evanfost@andrew.cmu.edu>
2026-06-10 14:46:57 -0400

    create script for launching robot quickly

---
+++ run_real.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+if [ -n "$TMUX" ]; then
+    echo "Running in TMUX not supported to avoid having to deal with potential complications"
+    exit 1
+fi
+
+if [[ $(tmux has-session -t "gopro-process") == 0 ]]; then
+    echo "GoPro server is running. You can attach to it using 'tmux attach-session -t gopro-process'"
+else
+    echo "No GoPro server is running. Starting a new session..."
+    tmux new-session -d -s "gopro-process"
+    tmux send-keys -t "gopro-process" "echo \"Enter your password, then press Ctrl-B, then d to exit\"" C-m
+    # taken from the launch script in the GoPro-ROS2 node (scripts/webcam_service.sh)
+    tmux send-keys -t "gopro-process" "sudo gopro webcam -n -p enp* && \
+        tmux wait-channel -S \"gopropwd\" && \
+        ffmpeg -nostdin -threads 1 -i 'udp://@0.0.0.0:8554?overrun_nonfatal=1&fifo_size=50000000' -f:v mpegts -fflags nobuffer -vf format=yuv420p -f v4l2 /dev/video42" C-m
+    tmux attach-session -t "gopro-process"
+    tmux wait-channel -S "gopropwd"
+    if [[ tmux capture-pane -p -t "session_name:window.pane" | grep "Error while starting the Webcam mode" ]]; then
+        echo "GoPro server may have failed to start. Check it out at 'tmux attach-session -t gopro-process'"
+        exit 1
+    fi
+    echo "GoPro server is now running. You can attach to it using 'tmux attach-session -t gopro-process'"
+fi
+
+# kill any existing session that exists
+tmux kill-session -t ros2-xarm 2>/dev/null; tmux new -s ros2-xarm
+
+SOURCE_COMMANDS="source /opt/ros/humble/setup.bash && source install/setup.bash"
+
+# launch gopro node
+tmux new-window -t ros2-xarm -n "gopro"
+tmux send-keys -t "ros2-xarm:gopro" "$SOURCE_COMMANDS" C-m
+tmux send-keys -t "ros2-xarm:gopro" "ros2 launch camera_cpp go_pro_launch.py" C-m
+
+# launch planning node
+tmux new-window -t ros2-xarm -n "planning"
+tmux send-keys -t "ros2-xarm:planning" "$SOURCE_COMMANDS" C-m
+tmux send-keys -t "ros2-xarm:planning" "ros2 launch xarm6_pointer planning_env.launch.py is_live:=true robot_ip:=192.168.1.213" C-m
+
+# launch control node
+tmux new-window -t ros2-xarm -n "controller"
+tmux send-keys -t "ros2-xarm:controller" "$SOURCE_COMMANDS" C-m
+tmux send-keys -t "ros2-xarm:controller" "ros2 launch xarm6_pointer pointer_node.launch.py controllers_name:=controllers" C-m
+
+# launch weed detection node
+tmux new-window -t ros2-xarm -n "weed"
+tmux send-keys -t "ros2-xarm:weed" "$SOURCE_COMMANDS" C-m
+tmux send-keys -t "ros2-xarm:weed" "ros2 launch weed_detection weed_detection.launch.py" C-m
+
+echo "Everything should be running, you can view startup logs in windows in 'tmux attach-session -t ros2-xarm' (Ctrl-B p/n to switch windows)"
